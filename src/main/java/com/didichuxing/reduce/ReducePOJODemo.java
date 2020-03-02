854b3bcd74888b238c0e03d2fdd59b3ea5562aac
gky <kyguo_zjut@163.com>
2020-03-02 17:54:57 +0800

    ReducePOJODemo add description

--- src/main/java/com/didichuxing/reduce/ReducePOJODemo.java
+++ src/main/java/com/didichuxing/reduce/ReducePOJODemo.java
@@ -5,6 +5,19 @@ import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+/**目的：类似spark的reduce功能，对输入的数据进行聚合操作，前一条数据和后一条数据进行聚合
+ **注意：输入和输出数据类型一定要一致，本案例中输入B类型，输出表类型
+ * 输入：spark 1
+ *      spark 2
+ *      hadoop 3
+ *      hadoop 4
+ *      spark 5
+ * 输出：1> B( spark, 1 )
+ *      1> B( spark, 3 )
+ *      4> B( hadoop, 3 )
+ *      4> B( hadoop, 7 )
+ *      1> B( spark, 8 )
+ */
 
 public class ReducePOJODemo {
     public static void main(String[] args) throws Exception {
